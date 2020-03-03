b092824540c6e088026296a6d312dc3f200eeed7
Magnus Lauritzen Holtet <magnulho@gmail.com>
2020-03-03 02:07:59 +0100

    Add font installation.

--- fonts/setup.py
+++ fonts/setup.py
@@ -1,5 +1,17 @@
 import os
 from pathlib import Path
+from distutils.dir_util import copy_tree
 
 print("> Installing Fonts")
 
+ignored = [".git"]
+folders = []
+for file in os.listdir("."):
+    if os.path.isdir(os.path.abspath(file)) and file not in ignored:
+        folders.append(file)
+
+for folder in folders:
+    print(f"\t* Installing {folder}")
+    src = os.path.abspath(folder)
+    dst = f"{Path.home()}/Library/Fonts"
+    copy_tree(src, dst)
